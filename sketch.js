c5453d6803ce8be741f0e5defb330a3f3cad0572
Ileini Melendez <122940019+ileini-melendez@users.noreply.github.com>
2023-03-16 16:19:13 +0000

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+  createCanvas(600, 500);
+}
+
+function draw() {
+  background(230);
+  // Delete the slashes on the next line to "uncomment" the ellipse
+  // ellipse(50, 50, 50, 50)
+
+  // Code noFill() function below 
+
+
+  // Code strokeWeight() function below
+  
+
+  // First circle (Blue)
+  
+
+  // Second circle (Yellow)
+  
+
+  // Third circle (Black)
+  
+
+  // Fourth circle (Green)
+  
+
+  // Fifth circle (Red)
+  
+}
