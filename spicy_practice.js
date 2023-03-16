83feb63daeec5b9fa4013c8c842958d6da1733e0
ileini-melendez <122940019+ileini-melendez@users.noreply.github.com>
2023-03-16 13:00:26 -0400

    Update spicy_practice.js

--- spicy_practice.js
+++ spicy_practice.js
@@ -1,1 +1,2 @@
-
+// Intro to Code with p5.js: Shapes, Color & Position
+// Use this file to complete the "Spicy Practice": Use the skills from this lesson to create a scene - some place you'd like to visit. This might be a real or an imaginary place, and it might be a place you've been many times, or only ever seen in photos (like in the movie Up). Whatever the case, use your imagination to create something that excites or inspires you.
