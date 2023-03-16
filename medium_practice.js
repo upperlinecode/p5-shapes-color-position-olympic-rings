d9b6de56e78f554303bbb9229be49f2f12441b31
ileini-melendez <122940019+ileini-melendez@users.noreply.github.com>
2023-03-16 13:32:03 -0400

    Update medium_practice.js

--- medium_practice.js
+++ medium_practice.js
@@ -1,2 +1,15 @@
 // Intro to Code with p5.js: Shapes, Color and Position
 // Use this file to complete the "Medium Practice": Use the skills from this lesson to create an emoji.
+
+
+
+function setup(){
+  createCanvas(800, 600);
+  
+}
+
+function draw(){
+  background(220);
+
+  
+}
